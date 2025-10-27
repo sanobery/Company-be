06e0f17110c5525eda7c5d85dca6857b96333aef
“sanobery” <sanobery@mindfiresolutions.com>
2025-10-27 09:36:05 +0530

    Feat: deployment changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,22 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::session",
+    config: {
+      cookie: {
+        secure: process.env.COOKIE_SECURE === "true", // set to false in dev
+        httpOnly: true,
+        maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
